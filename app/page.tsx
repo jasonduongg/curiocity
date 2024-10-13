2248722842c2391bd604cf9a4cb38c807c728303
jasonduong-portfolio <jasonduong.website@gmail.com>
2024-10-13 14:19:49 -0700

    merged tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,48 @@ import CustomSwitch from "@/components/CustomSwitch";
 export default function Home() {
   return (
     <div className="flex h-screen flex-col items-center justify-center space-y-4">
-      <h1 className="text-2xl">
-        <span className="text-wdbBlue">Web Development</span> at Berkeley
-      </h1>
-      <p>
-        Full-stack web development project template. Check the README for more
-        information!
-      </p>
+      <h1 className="text-2xl">Curiocity x WDB</h1>
       <CustomSwitch />
+      <div className="flex">
+        <div className="h-16 w-32 bg-fileRed text-textPrimary"> fileRed </div>
+        <div className="h-16 w-32 bg-fileOrange text-textPrimary">
+          {" "}
+          fileOrange{" "}
+        </div>
+        <div className="h-16 w-32 bg-fileBlue text-textPrimary"> fileBlue</div>
+        <div className="h-16 w-32 bg-fileLightTeal text-textPrimary">
+          {" "}
+          fileLightTeal
+        </div>
+        <div className="h-16 w-32 bg-fileGreen text-textPrimary">
+          {" "}
+          fileGreen{" "}
+        </div>
+        <div className="h-16 w-32 bg-accentPrimary text-textPrimary">
+          {" "}
+          accentPrimary{" "}
+        </div>
+        <div className="h-16 w-32 bg-accentSecondary text-textPrimary">
+          {" "}
+          accentSecondary{" "}
+        </div>
+        <div className="h-16 w-32 bg-bgPrimary text-textPrimary">
+          {" "}
+          bgPrimary{" "}
+        </div>
+        <div className="h-16 w-32 bg-bgSecondary text-textPrimary">
+          {" "}
+          bgSecondary{" "}
+        </div>
+        <div className="h-16 w-32 bg-textPrimary text-textSecondary">
+          {" "}
+          textPrimary{" "}
+        </div>
+        <div className="h-16 w-32 bg-textSecondary text-textPrimary">
+          {" "}
+          textSecondary{" "}
+        </div>
+      </div>
     </div>
   );
 }
