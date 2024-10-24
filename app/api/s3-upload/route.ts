0111d59d8ccac7ad64fb86f58c3783cc64e557c9
Richa Jos <richajos34@berkeley.edu>
2024-10-24 06:05:58 -0700

    final adding notes

--- app/api/s3-upload/route.ts
+++ app/api/s3-upload/route.ts
@@ -1,5 +1,5 @@
 import { S3 } from "aws-sdk";
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
 const s3 = new S3({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -8,26 +8,29 @@ const s3 = new S3({
 });
 
 export async function POST(req: Request) {
-  try{
+  try {
     const body = await req.json();
     const { fileName, fileType } = body;
     console.log("Parsed request body:", { fileName, fileType });
 
     const s3Params = {
-        Bucket: process.env.NEXT_PUBLIC_S3_UPLOAD_BUCKET,
-        Key: fileName,
-        Expires: 60,
-        ContentType: fileType,
-      };
+      Bucket: process.env.NEXT_PUBLIC_S3_UPLOAD_BUCKET,
+      Key: fileName,
+      Expires: 60,
+      ContentType: fileType,
+    };
 
-      const signedUrl = await s3.getSignedUrlPromise("putObject", s3Params);
+    const signedUrl = await s3.getSignedUrlPromise("putObject", s3Params);
 
-      return NextResponse.json({ url: signedUrl }, { status: 200 });
-  } catch(error) {
-    return NextResponse.json({ error: "Failed to create S3 signed URL" }, { status: 500 });
+    return NextResponse.json({ url: signedUrl }, { status: 200 });
+  } catch {
+    return NextResponse.json(
+      { error: "Failed to create S3 signed URL" },
+      { status: 500 },
+    );
   }
 }
 
 export async function OPTIONS() {
-    return NextResponse.json({ error: "Method not allowed" }, { status: 405 });
-  }
+  return NextResponse.json({ error: "Method not allowed" }, { status: 405 });
+}
