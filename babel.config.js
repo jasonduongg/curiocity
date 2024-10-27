a8d3556c659e83eeea7de6c36cf8da5734af3934
ChrisW0524 <chris.wang.wcy@gmail.com>
2024-10-26 17:31:49 -0700

    Added NavBar Tests

---
+++ babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  presets: [
+    "@babel/preset-env", // Transforms modern JavaScript features
+    "@babel/preset-react", // Handles JSX and React code
+    "@babel/preset-typescript", // Handles TypeScript
+  ],
+};
