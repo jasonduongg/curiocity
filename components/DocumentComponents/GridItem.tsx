5c0cab933738953a77246c55af8e7f883b36d22a
jasonduong-portfolio <jasonduong.website@gmail.com>
2024-11-11 15:51:44 -0800

    grid item

--- components/DocumentComponents/GridItem.tsx
+++ components/DocumentComponents/GridItem.tsx
@@ -1,4 +1,3 @@
-
 function stripHtmlTags(text: string) {
   return text.replace(/<\/?[^>]+(>|$)/g, "");
 }
@@ -25,7 +24,7 @@ export default function GridItem({ title, text, dateAdded, onClick }: Props) {
       className="flex h-64 w-full max-w-full cursor-pointer flex-col justify-between rounded-lg bg-zinc-800 p-4 shadow-md hover:bg-red-300"
       onClick={onClick}
     >
-      <div className="flex h-64 w-48 min-w-48 flex-col items-stretch rounded-xl border-[1px] border-textSecondary">
+      <div className="flex h-full w-full min-w-48 flex-col items-stretch rounded-xl border-[1px] border-textSecondary">
         <div className="grow overflow-y-hidden p-2">
           <p className="text-xs text-white">{stripHtmlTags(text)}</p>
         </div>
